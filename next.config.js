9273560ce712f3717dc6c8828c16b5f7fd75d0f0
Sh031224 <1cktmdgh2@gmail.com>
2021-11-24 17:49:36 +0900

    add: polyfills

--- next.config.js
+++ next.config.js
@@ -5,4 +5,21 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
   webpack5: true,
+  webpack: function (config) {
+    const originalEntry = config.entry;
+
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (
+        entries["main.js"] &&
+        !entries["main.js"].includes("./src/polyfills.js")
+      ) {
+        entries["main.js"].unshift("./src/polyfills.js");
+      }
+      return entries;
+    };
+
+    return config;
+  },
 };
